f7867fac1531deef7451077ecdeb5e6357d407f7
Brunix2018 <42790138+Brunix2018@users.noreply.github.com>
2023-07-01 19:46:57 -0300

    pruebas

---
+++ src/AimaTest.py
@@ -0,0 +1,43 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Import libraries
+import aima3.utils
+import aima3.logic
+from aima3.logic import pl_resolution
+
+
+# The main entry point for this module
+def main():
+    # Create an array to hold clauses
+    clauses = []
+    # Add first-order logic clauses (rules and fact)
+
+    clauses.append(aima3.utils.expr("Nombre(Bruno)"))
+    clauses.append(aima3.utils.expr("Nombre(Bruno) ==> Puesto(Ceo)"))
+
+    clauses.append(aima3.utils.expr("Lenguaje(x) ==> LengUser(x)"))
+    clauses.append(aima3.utils.expr("Lenguaje(Java)"))
+    # Create a first-order logic knowledge base (KB) with clauses
+    KB = aima3.logic.FolKB(clauses)
+    # Add rules and facts with tell
+
+    # Get information from the knowledge base with ask
+    consulta = aima3.utils.expr("LengUser(Pepe)")
+
+    if pl_resolution(KB, consulta):
+        print("La afirmación es verdadera")
+    else:
+        print("La afirmación es falsa")
+
+    print("KB.ask= ", KB.ask(consulta))
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
